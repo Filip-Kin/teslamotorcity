0fdf2b69edf45182263d991171109f917ca273ff
Filip Kin <filip@kinmails.com>
2020-01-23 15:47:59 -0500

    private repo so i can have shitty commits yay

--- compile-scss.js
+++ compile-scss.js
@@ -1,3 +1,5 @@
+const minify = require('uglifycss');
+
 require('node-sass').render({
     file: 'node_modules/materialize-css/sass/materialize.scss'
 }, function(err, result) { 
@@ -5,8 +7,10 @@ require('node-sass').render({
     let out = result.css.toString();
     console.log(out);
     require('fs').writeFileSync('node_modules/materialize-css/css/materialize.css', out);
-    let min = require('uglifycss').processString(out)
+    let min = minify.processString(out)
+    let globalMin = minify.processFiles(['web/css/global.css']);
     console.log(min);
+    require('fs').writeFileSync('web/css/global.min.css', globalMin);
     require('fs').writeFileSync('node_modules/materialize-css/css/materialize.min.css', min);
     console.log('Done!');
 });
