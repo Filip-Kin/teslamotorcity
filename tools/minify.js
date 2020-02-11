27d6757381835827645041a88da73abcf23714fb
Filip Kin <filip@kinmails.com>
2020-02-11 14:56:26 -0500

    commit msg

--- tools/minify.js
+++ tools/minify.js
@@ -1,4 +1,19 @@
-const minify = require('uglifycss');
-let globalMin = minify.processFiles(['web/css/global.css']);
-require('fs').writeFileSync('web/css/global.min.css', globalMin);
-console.log('Done!');
+const { writeFile, readFileSync } = require('fs');
+const minifyCSS = require('uglifycss');
+const minifyJS = require('uglify-js');
+
+Promise.all([
+    new Promise((resolve, reject) => {
+        writeFile('static/css/global.min.css', minifyCSS.processFiles(['static/css/global.css']), () =>  resolve());
+    }),
+    new Promise((resolve, reject) => {
+        let minified = minifyJS.minify({
+            'add.js': readFileSync('static/js/add.js'),
+            'admin.js': readFileSync('static/js/admin.js'),
+            'index.js': readFileSync('static/js/index.js'),
+            'inventory.js': readFileSync('static/js/inventory.js')
+        });
+        if (minified.error) console.error(minified.error);
+        writeFile('static/js/global.min.js', minified.code, () => resolve());
+    })
+]).then(() => console.log('Done!'));
