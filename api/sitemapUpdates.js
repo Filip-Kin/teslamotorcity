da43750a1cd7c31bcbee8dfc2bd5156560c64f5e
Filip Kin <filip@kinmails.com>
2020-02-24 13:33:03 -0500

    plz work

--- api/sitemapUpdates.js
+++ api/sitemapUpdates.js
@@ -5,31 +5,39 @@ let xmlObject = xml.xml2json(readFileSync('./static/sitemap.xml'), {compact: tru
 console.log(xmlObject);
 
 exports.updateCar = (id) => {
-    let car = xmlObject.urlset.url.find((v) => (v.loc === 'https://www.starmotorsales.net/car/'+id));
+    let car = undefined;
+    for (let i = 0; i < xmlObject.urlset.url.length; i++) {
+        console.log(xmlObject.urlset.url[i]);
+        if (xmlObject.urlset.url[i].loc._text === 'https://www.starmotorsales.net/car/'+id) car = i;
+    }
     let today = new Date();
     let lastmod = today.getFullYear()+'-'+today.getMonth()+'-'+today.getDate();
 
     if (car == undefined) {
         xmlObject.urlset.url.push({
-            loc: 'https://www.starmotorsales.net/car/'+id,
-            lastmod: lastmod,
-            changefreq: 'weekly'
+            loc: { _text: 'https://www.starmotorsales.net/car/'+id },
+            lastmod: { _text: lastmod },
+            changefreq: { _text: 'weekly' }
         });
     } else {
-        xmlObject.urlset[xmlObject.urlset.url.indexOf(car)].lastmod = lastmod;
+        xmlObject.urlset.url[car].lastmod = { _text: lastmod };
     }
-    xmlObject.urlset[1].lastmod = lastmod;
+    xmlObject.urlset[1].lastmod = { _text: lastmod };
 
     writeFileSync('./static/sitemap.xml', xml.json2xml(xmlObject, {compact: true, ignoreComment: true, spaces: 4}));
 }
 
 exports.removeCar = (id) => {
-    let car = xmlObject.urlset.url.find((v) => (v.loc === 'https://www.starmotorsales.net/car/'+id));
+    let car = undefined;
+    for (let i = 0; i < xmlObject.urlset.url.length; i++) {
+        console.log(xmlObject.urlset.url[i]);
+        if (xmlObject.urlset.url[i].loc._text === 'https://www.starmotorsales.net/car/'+id) car = i;
+    }
     let today = new Date();
     let lastmod = today.getFullYear()+'-'+today.getMonth()+'-'+today.getDate();
 
-    xmlObject.urlset = xmlObject.urlset.url.filter((v) => (v !== car));
-    xmlObject.urlset[1].lastmod = lastmod;
+    xmlObject.urlset.slice(i, 1);
+    xmlObject.urlset[1].lastmod = { _text: lastmod };
 
     writeFileSync('./static/sitemap.xml', xml.json2xml(xmlObject, {compact: true, ignoreComment: true, spaces: 4}));
 }
