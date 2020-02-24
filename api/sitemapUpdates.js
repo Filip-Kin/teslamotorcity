bce138b4ca5521cb033b38b20681762820c11c08
Filip Kin <filip@kinmails.com>
2020-02-24 13:25:51 -0500

    im bad at this

--- api/sitemapUpdates.js
+++ api/sitemapUpdates.js
@@ -5,18 +5,18 @@ let xmlObject = xml.xml2json(readFileSync('./static/sitemap.xml'), {compact: tru
 console.log(xmlObject);
 
 exports.updateCar = (id) => {
-    let car = xmlObject.urlset.urlfind((v) => (v.loc === 'https://www.starmotorsales.net/car/'+id));
+    let car = xmlObject.urlset.url.find((v) => (v.loc === 'https://www.starmotorsales.net/car/'+id));
     let today = new Date();
     let lastmod = today.getFullYear()+'-'+today.getMonth()+'-'+today.getDate();
 
     if (car == undefined) {
-        xmlObject.urlset.urlpush({
+        xmlObject.urlset.url.push({
             loc: 'https://www.starmotorsales.net/car/'+id,
             lastmod: lastmod,
             changefreq: 'weekly'
         });
     } else {
-        xmlObject.urlset[xmlObject.urlset.urlindexOf(car)].lastmod = lastmod;
+        xmlObject.urlset[xmlObject.urlset.url.indexOf(car)].lastmod = lastmod;
     }
     xmlObject.urlset[1].lastmod = lastmod;
 
@@ -24,11 +24,11 @@ exports.updateCar = (id) => {
 }
 
 exports.removeCar = (id) => {
-    let car = xmlObject.urlset.urlfind((v) => (v.loc === 'https://www.starmotorsales.net/car/'+id));
+    let car = xmlObject.urlset.url.find((v) => (v.loc === 'https://www.starmotorsales.net/car/'+id));
     let today = new Date();
     let lastmod = today.getFullYear()+'-'+today.getMonth()+'-'+today.getDate();
 
-    xmlObject.urlset = xmlObject.urlset.urlfilter((v) => (v !== car));
+    xmlObject.urlset = xmlObject.urlset.url.filter((v) => (v !== car));
     xmlObject.urlset[1].lastmod = lastmod;
 
     writeFileSync('./static/sitemap.xml', xml.json2xml(xmlObject, {compact: true, ignoreComment: true, spaces: 4}));
