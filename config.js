e86e97c5f2b223c2ba64558b3837170e6941ac9d
Filip9696 <me@filipkin.com>
2021-12-14 13:12:54 -0500

    don't leak the database credentials

--- config.js
+++
@@ -1,7 +0,0 @@
-exports.DATABASE_CREDENTIALS = {
-    host: 'server.filipkin.com',
-    port: 3306,
-    user: 'u1_k7xcLijw2h',
-    password: '=5g5EUxgGNZ3sW5Tts!9o22!',
-    database: 's1_teslamotorcity'
-};
