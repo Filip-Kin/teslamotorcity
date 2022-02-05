2c25d28dba306593c467ca4089381235735f785f
Filip Kin <filip@kinmails.com>
2022-02-05 16:31:00 -0500

    edit

--- static/js/index.js
+++ static/js/index.js
@@ -5,17 +5,22 @@ document.addEventListener('DOMContentLoaded', function() {
 let nav_opaque = false;
 document.addEventListener('scroll', () => {
     if ((
-        (window.innerWidth > 992 && window.scrollY > 600) ||
-        (window.innerWidth < 992 && window.scrollY > 250))
-         && !nav_opaque) {
+            (window.innerWidth > 992 && window.scrollY > 600) ||
+            (window.innerWidth < 992 && window.scrollY > 250)) &&
+        !nav_opaque) {
         document.querySelector('nav').classList.remove('clear')
+        document.querySelector('.menu-button').style.color = '#fff';
+        document.querySelector('.brand-logo').style.color = '#fff';
+        document.querySelector('.phone-button').style.color = '#fff';
         nav_opaque = true;
-    }
-    else if ((
-        (window.innerWidth > 992 && window.scrollY < 600) || 
-        (window.innerWidth < 992 && window.scrollY < 250)) 
-        && nav_opaque) {
+    } else if ((
+            (window.innerWidth > 992 && window.scrollY < 600) ||
+            (window.innerWidth < 992 && window.scrollY < 250)) &&
+        nav_opaque) {
         document.querySelector('nav').classList.add('clear');
+        document.querySelector('.menu-button').style.color = '#000';
+        document.querySelector('.brand-logo').style.color = '#000';
+        document.querySelector('.phone-button').style.color = '#000';
         nav_opaque = false;
     }
 });
@@ -24,5 +29,8 @@ if (
     (window.innerWidth > 992 && window.scrollY > 600) ||
     (window.innerWidth < 992 && window.scrollY > 250)) {
     document.querySelector('nav').classList.remove('clear');
+    document.querySelector('.menu-button').style.color = '#fff';
+    document.querySelector('.brand-logo').style.color = '#fff';
+    document.querySelector('.phone-button').style.color = '#fff';
     nav_opaque = true;
 }
